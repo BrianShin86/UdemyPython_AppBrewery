03b493e7548eb5daad8503f0f6d6af451cb40cab
Brian Shin <87712603+BrianShin86@users.noreply.github.com>
2021-12-01 20:43:42 +0000

    challenge 1 update

--- main.py
+++ main.py
@@ -2,22 +2,42 @@
 
 #Strings
 
-print("Hello"[4])
+# print("Hello"[4])
 
-print("123" + "345")
+# print("123" + "345")
 
 #Integer
 
-print(123+345)
+# print(123+345)
 
-123_456_789
+# 123_456_789
 
 # Float
 
-3.14159
+# 3.14159
 
 # Boolean
 
-True
-False
+# True
+# False
 
+# num_char = len(input("What is your name?"))
+# new_num_char = str(num_char)
+# print("Your name has " + new_num_char + " characters.")
+
+# a= float(123)
+# print(type(a))
+
+# print(70 + float("100.5"))
+# print(str(70) + str(100))
+
+
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+A = two_digit_number [0]
+B = two_digit_number [1]
+print (int(A) + int(B))
