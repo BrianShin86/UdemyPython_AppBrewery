98e03e3d035b196c6333997aa951a7650fcbc805
Brian Shin <87712603+BrianShin86@users.noreply.github.com>
2021-12-02 21:53:26 +0000

    Challenge 1

--- main.py
+++ main.py
@@ -5,10 +5,25 @@
 #   print("Continue")
 
 # Practice 1
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
 
-if height >= 120:
-  print("You can ride the rollercoaster!")
-else:
-  print("Sorry, you have to grow taller before you can ride.")
+# if height >= 120:
+#   print("You can ride the rollercoaster!")
+# else:
+#   print("Sorry, you have to grow taller before you can ride.")
+
+
+# Challenge 1
+# 🚨 Don't change the code below 👇
+# number = int(input("Which number do you want to check? "))
+# # 🚨 Don't change the code above 👆
+
+# #Write your code below this line 👇
+# if number%2 == 0:
+#     print("This is an even number.")
+# else:
+#     print("This is an odd number.")
+
+
+
