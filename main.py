e5e510e2487211aa47a35a65571d83ac6885a74e
Brian Shin <87712603+BrianShin86@users.noreply.github.com>
2021-12-02 22:29:22 +0000

    Challenge 2

--- main.py
+++ main.py
@@ -26,4 +26,57 @@
 #     print("This is an odd number.")
 
 
+# # Practice 2
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
 
+# if height >= 120:
+#   print("You can ride the rollercoaster!")
+#   age = int(input("What is your age? "))
+#   if age <= 18:
+#     print("Please pay $7.")
+#   else:
+#     print("Please pay $12")
+# else:
+#   print("Sorry, you have to grow taller before you can ride.")
+
+
+# # Practice 3
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
+
+# if height >= 120:
+#   print("You can ride the rollercoaster!")
+#   age = int(input("What is your age? "))
+#   if age < 12:
+#     print("Please pay $5.")
+#   elif 12 <= 18:
+#     print("Please pay $7.")
+#   else:
+#     print("Please pay $12")
+# else:
+#   print("Sorry, you have to grow taller before you can ride.")
+
+
+# # challenge 2
+# # 🚨 Don't change the code below 👇
+# height = float(input("enter your height in m: "))
+# weight = float(input("enter your weight in kg: "))
+# # 🚨 Don't change the code above 👆
+
+# #Write your code below this line 👇
+# bmi = weight / (height ** 2)
+# bmi_r = round(bmi, 0 )
+# bmi_i = int(bmi_r)
+# if bmi_i < 18.5:
+#     print(f"Your BMI is {bmi_i}, you are underweight.")
+# elif bmi_i < 25:
+#     print(f"Your BMI is {bmi_i}, you have a normal weight.")
+# elif bmi_i < 30:
+#     print(f"Your BMI is {bmi_i}, you are slightly overweight.")
+# elif bmi_i <= 35:
+#     print(f"Your BMI is {bmi_i}, you are obese.")
+# else:
+#     print(f"Your BMI is {bmi_i}, you are clinically obese.")
+
+Challenge 3
