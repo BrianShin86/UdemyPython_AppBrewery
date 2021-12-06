03d5a66138685a8f80b8c307bdeb031604062b7f
Brian Shin <87712603+BrianShin86@users.noreply.github.com>
2021-12-06 10:11:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ def my_function():
   print("Hello")
   print("Bye")
 
+for itme in list_of_items:
+  # do someting
+for number in range(a,b):
+  print(number)
+  
+while something_is_true
+  #Do someting repeatedly
