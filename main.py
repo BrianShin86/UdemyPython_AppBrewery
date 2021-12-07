1ea0057083c37b1a7dc08a5eb9871ff1915aba54
Brian Shin <87712603+BrianShin86@users.noreply.github.com>
2021-12-07 16:52:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -208,7 +208,7 @@ while not end_of_game:
     
     
 #Step 5
-
+from replit import clear
 import random
 
 #TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
@@ -233,7 +233,7 @@ for _ in range(word_length):
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
-    
+    clear()
     #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
     if guess in display:
       print(f"You've already guessed {guess}")
@@ -264,8 +264,3 @@ while not end_of_game:
 
     #TODO-2: - Import the stages from hangman_art.py and make this error go away.
     print(stages[lives])
-
-
-
-
-
