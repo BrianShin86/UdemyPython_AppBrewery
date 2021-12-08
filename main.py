deec637871f8940da2748244bf220a546d56dd39
Brian Shin <87712603+BrianShin86@users.noreply.github.com>
2021-12-08 09:49:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
-# Review: 
-# Create a function called greet(). 
-# Write 3 print statements inside the function.
-# Call the greet() function and run your code.
-
-# def greet():
-#   print(f"Hello Brian")
-#   print(f"How do you do Brian?")
-#   print("About to start!")
+#Simple Function
+def greet():
+  print("Hello Angela")
+  print("How do you do Jack Bauer?")
+  print("Isn't the weather nice today?")
+greet()
 
 #Function that allows for input
+#'name' is the parameter.
+#'Jack Bauer' is the argument.
 def greet_with_name(name):
   print(f"Hello {name}")
   print(f"How do you do {name}?")
-  print("About to start!")
+greet_with_name("Jack Bauer")
 
-greet_with_name("Bill")
+#Functions with more than 1 input
+def greet_with(name, location):
+  print(f"Hello {name}")
+  print(f"What is it like in {location}?")
+
+#Calling greet_with() with Positional Arguments
+greet_with("Jack Bauer", "Nowhere")
+#vs.
+greet_with("Nowhere", "Jack Bauer")
+
+
+#Calling greet_with() with Keyword Arguments
+greet_with(location="London", name="Angela")
