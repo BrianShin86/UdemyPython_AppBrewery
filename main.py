bf028dbe188d5bdbf68361e49a1fe10da9f80d1b
Brian Shin <87712603+BrianShin86@users.noreply.github.com>
2021-12-08 14:36:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+# def greet():
+#   print(f"Hello Brian")
+#   print(f"How do you do Brian?")
+#   print("About to start!")
+
+#Function that allows for input
+def greet_with_name(name):
+  print(f"Hello {name}")
+  print(f"How do you do {name}?")
+  print("About to start!")
+
+greet_with_name("Bill")
